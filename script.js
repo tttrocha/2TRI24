f762aea4d251dc818429e7b262dde11dc6b8336a
tttrocha <108011467+tttrocha@users.noreply.github.com>
2024-08-19 19:30:46 -0300

    UMTAMFIF65656

--- script.js
+++ script.js
@@ -6,15 +6,15 @@ const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, ele também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento?",
+        enunciado: "THE UMBRELLA ACADEMY",
         alternativas: [
             {
-                texto: "Isso é assustador!",
-                afirmacao: "afirmacao"
+                texto: "4 TEMPORADA DE THE UMBRELLA ACADEMY",
+                afirmacao: "CINCO"
             },
             {
-                texto: "Isso é maravilhoso!",
-                afirmacao: "afirmacao"
+                texto: "TRES!",
+                afirmacao: "UM"
             }           
             
         ]
